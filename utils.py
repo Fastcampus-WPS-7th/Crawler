46d12dc6cbfcaf4c0dabbf2ee5e64d56b3378cb8
LeeHanYeong <dev@azelf.com>
2018-01-25 16:55:21 +0900

    get_top100_list()함수 구현 완료 및 main모듈 정리

--- utils.py
+++ utils.py
@@ -1,19 +1,17 @@
+import os
 import re
 
-import os
 import requests
+from bs4 import BeautifulSoup
 
 
 def get_top100_list(refresh_html=False):
     """
     실시간 차트 1~100위의 리스트 반환
     파일위치:
-        현재 파일(모듈)의 위치를 사용한 상위 디렉토리 경로 (crawler디렉토리):
-            os.path.dirname(os.path.abspath(__name__))
-
-        1~50위:   data/chart_realtime_50.html
-        51~100위: data/chart_realtime_100.html
-    :return:
+        data/chart_realtime.html
+    :param refresh_html: True일 경우, 무조건 새 HTML파일을 사이트에서 받아와 덮어씀
+    :return: 곡 정보 dict의 list
     """
     # utils가 있는
     path_module = os.path.abspath(__name__)
@@ -46,28 +44,35 @@ def get_top100_list(refresh_html=False):
     except FileExistsError:
         print(f'"{file_path}" file is already exists!')
 
-    # file_path = os.path.join(path_data_dir, 'abc.txt')
-    # print(f'file_path: \n{file_path}')
+    # 1. source변수에 위에 정의해놓은 file_path(data/chart_realtime.html)의
+    #       파일 내용을 읽어온 결과를 할당
+    f = open(file_path, 'rt')
+    source = f.read()
+    f.close()
+    # 2. soup변수에 BeautifulSoup클래스 호출에 source를 전달해 만들어진 인스턴스를 할당
+    #    soup = BeautifulSoup(source)
+    soup = BeautifulSoup(source, 'lxml')
+    # 3. BeautifulSoup을 사용해 HTML을 탐색하며 dict의 리스트를(result) 생성, 마지막에 리턴
 
-    # result = []
-    # for tr in soup.find_all('tr', class_='lst50'):
-    #     rank = tr.find('span', class_='rank').text
-    #     title = tr.find('div', class_='rank01').find('a').text
-    #     artist = tr.find('div', class_='rank02').find('a').text
-    #     album = tr.find('div', class_='rank03').find('a').text
-    #     url_img_cover = tr.find('a', class_='image_typeAll').find('img').get('src')
-    #     # http://cdnimg.melon.co.kr/cm/album/images/101/28/855/10128855_500.jpg/melon/resize/120/quality/80/optimize
-    #     # .* -> 임의 문자의 최대 반복
-    #     # \. -> '.' 문자
-    #     # .*?/ -> '/'이 나오기 전까지의 최소 반복
-    #     p = re.compile(r'(.*\..*?)/')
-    #     url_img_cover = re.search(p, url_img_cover).group(1)
-    #
-    #     result.append({
-    #         'rank': rank,
-    #         'title': title,
-    #         'url_img_cover': url_img_cover,
-    #         'artist': artist,
-    #         'album': album,
-    #     })
-    #
+    result = []
+    for tr in soup.find_all('tr', class_=['lst50', 'lst100']):
+        rank = tr.find('span', class_='rank').text
+        title = tr.find('div', class_='rank01').find('a').text
+        artist = tr.find('div', class_='rank02').find('a').text
+        album = tr.find('div', class_='rank03').find('a').text
+        url_img_cover = tr.find('a', class_='image_typeAll').find('img').get('src')
+        # http://cdnimg.melon.co.kr/cm/album/images/101/28/855/10128855_500.jpg/melon/resize/120/quality/80/optimize
+        # .* -> 임의 문자의 최대 반복
+        # \. -> '.' 문자
+        # .*?/ -> '/'이 나오기 전까지의 최소 반복
+        p = re.compile(r'(.*\..*?)/')
+        url_img_cover = re.search(p, url_img_cover).group(1)
+
+        result.append({
+            'rank': rank,
+            'title': title,
+            'url_img_cover': url_img_cover,
+            'artist': artist,
+            'album': album,
+        })
+    return result
