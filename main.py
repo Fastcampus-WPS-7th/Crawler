fcd5491f36c4b7622cee2dc40e57f779c2c5e718
LeeHanYeong <dev@azelf.com>
2018-01-25 11:36:30 +0900

    get_tag_attribute, get_tag_content함수 작성

--- main.py
+++ main.py
@@ -6,6 +6,42 @@ PATTERN_DIV_RANK01 = re.compile(r'<div class="ellipsis rank01">.*?</div>', re.DO
 # <a href=....>(내용)</a>
 PATTERN_A_CONTENT = re.compile(r'<a.*?>(.*?)</a>')
 
+
+def get_tag_attribute(attribute_name, tag_string):
+    """
+    특정 tag문자열(tag_string)에서 attribute_name에 해당하는 속성의 값을 리턴하는 함수
+    :param attribute_name: 태그가 가진 속성명
+    :return: 속성이 가진 값
+    """
+    # 문자열 포맷에 이름 붙이고, format()에서 키워드인수로 전달
+    p = re.compile(r'^<.*?{attribute_name}="(?P<value>.*?)".*?>'.format(
+        attribute_name=attribute_name
+    ), re.DOTALL)
+    m = re.search(p, tag_string)
+    if m:
+        return m.group('value')
+    return ''
+
+
+# example1 = '<a href="https://google.co.kr">Google</a>'
+example1 = '<div><span class="sdf">ASDF</span></div>'
+get_tag_attribute('href', example1)
+
+
+def get_tag_content(tag_string):
+    """
+    특정 tag문자열(tag_string)이 가진 내용을 리턴
+    tag문자열이 스스로 열고 닫는 태그 (ex: img태그)일 경우엔 공백을 반환
+    :param tag_string:
+    :return:
+    """
+
+
+example2 = '<div>Content</div>'
+get_tag_content(example2)
+# -> Content
+
+
 # 로컬 HTML문서 불러오기
 source = open('melon.html', 'rt').read()
 
@@ -19,7 +55,6 @@ for match_div_rank01 in match_list:
     match_title = re.search(PATTERN_A_CONTENT, div_rank01_content)
     title = match_title.group(1)
     print(title)
-
 
 """
 숙제
@@ -38,3 +73,7 @@ print(chart) 했을 때
 이렇게 나오도록 나머지 정규표현식 구현을 완성
 """
 
+"""
+
+    
+"""
