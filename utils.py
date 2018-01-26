daf5b057ca41db0e36159cc41e6cef26f64990b9
LeeHanYeong <dev@azelf.com>
2018-01-26 15:00:12 +0900

    search_song()함수 parameter이름 변경

--- utils.py
+++ utils.py
@@ -167,15 +167,15 @@ def get_song_detail(song_id, refresh_html=False):
     }
 
 
-def search_song(title):
+def search_song(q):
     """
     곡 명으로 멜론에서 검색한 결과 리스트를 리턴
-    :param title: 검색할 곡 명
+    :param q: 검색할 곡 명
     :return: 결과 dict리스트
     """
     """
     1. http://www.melon.com/search/song/index.htm
-        에 q={title}, section=song으로 parameter를 준 URL에
+        에 q={q}, section=song으로 parameter를 준 URL에
         requests를 사용해 요청
     2. response.text를 사용해 BeautifulSoup인스턴스 soup생성
     3. soup에서 적절히 결과를 가공
@@ -185,12 +185,13 @@ def search_song(title):
     """
     url = 'https://www.melon.com/search/song/index.htm'
     params = {
-        'q': title,
+        'q': q,
         'section': 'song',
     }
     response = requests.get(url, params)
     soup = BeautifulSoup(response.text, 'lxml')
     tr_list = soup.select('form#frm_defaultList table > tbody > tr')
+    # tr_list = soup.find('form', id='frm_defaultList').find('table').find('tbody').find_all('tr')
 
     result = []
     for tr in tr_list:
