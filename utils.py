8f6e30da1fa9cca3303f34f589041478c431cab5
LeeHanYeong <dev@azelf.com>
2018-01-26 11:15:50 +0900

    get_song_detail()에서 파일다운로드 로직 구현

--- utils.py
+++ utils.py
@@ -4,6 +4,15 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
+# utils가 있는
+PATH_MODULE = os.path.abspath(__file__)
+
+# 프로젝트 컨테이너 폴더 경로
+ROOT_DIR = os.path.dirname(PATH_MODULE)
+
+# data/ 폴더 경로
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+
 
 def get_top100_list(refresh_html=False):
     """
@@ -13,26 +22,14 @@ def get_top100_list(refresh_html=False):
     :param refresh_html: True일 경우, 무조건 새 HTML파일을 사이트에서 받아와 덮어씀
     :return: 곡 정보 dict의 list
     """
-    # utils가 있는
-    path_module = os.path.abspath(__file__)
-    print(f'path_module: \n{path_module}')
-
-    # 프로젝트 컨테이너 폴더 경로
-    root_dir = os.path.dirname(path_module)
-    print(f'root_dir: \n{root_dir}')
-
-    # data/ 폴더 경로
-    path_data_dir = os.path.join(root_dir, 'data')
-    print(f'path_data_dir: \n{path_data_dir}')
-
     # 만약에 path_data_dir에 해당하는 폴더가 없을 경우 생성해준다
-    os.makedirs(path_data_dir, exist_ok=True)
+    os.makedirs(DATA_DIR, exist_ok=True)
 
     # 실시간 1~100위 웹페이지 주소
     url_chart_realtime = 'https://www.melon.com/chart/index.htm'
 
     # 실시간 1~100위 웹페이지 HTML을 data/chart_realtime.html 에 저장
-    file_path = os.path.join(path_data_dir, 'chart_realtime.html')
+    file_path = os.path.join(DATA_DIR, 'chart_realtime.html')
     try:
         # refresh_html매개변수가 True일 경우, wt모드로 파일을 열어 새로 파일을 다운받도록 함
         file_mode = 'wt' if refresh_html else 'xt'
@@ -81,7 +78,7 @@ def get_top100_list(refresh_html=False):
     return result
 
 
-def get_song_detail(song_id):
+def get_song_detail(song_id, refresh_html=False):
     """
     song_id에 해당하는 곡 정보 dict를 반환
     위의 get_top100_list의 각 곡 정보에도 song_id가 들어가도록 추가
@@ -92,7 +89,22 @@ def get_song_detail(song_id):
     파일명
         song_detail_{song_id}.html
 
-    :param song_id: 곡 정보 dict
-    :return:
+    :param song_id: Melon사이트에서 사용하는 곡의 고유 ID값
+    :param refresh_html: 이미 다운받은 HTML데이터가 있을 때 기존 데이터를 덮어씌울지 여부
+    :return: 곡 정보 dict
     """
-    pass
+    # 파일위치는 data/song_detail_{song_id}.html
+    file_path = os.path.join(DATA_DIR, f'song_detail_{song_id}.html')
+    try:
+        file_mode = 'wt' if refresh_html else 'xt'
+        with open(file_path, file_mode) as f:
+            # url과 parameter구분해서 requests사용
+            url = f'https://www.melon.com/song/detail.htm'
+            params = {
+                'songId': song_id,
+            }
+            response = requests.get(url, params)
+            source = response.text
+            f.write(source)
+    except FileExistsError:
+        print(f'"{file_path}" file is already exists!')
